c831219ca10d11cc32a462b63ca9d7fa069ca72d
zmijevik <zmijevik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-11 01:17:47 +0000

    fixed an error
    git-svn-id: https://plugins.svn.wordpress.org/supra-csv-parser/trunk@584117 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- supra_csv_postmeta.php
+++ supra_csv_postmeta.php
@@ -1,4 +1,11 @@
 <?php 
+require_once('classes/Debug.php');
+require_once('classes/Presets.php');
+require_once('classes/SupraCsvPostMeta.php');
+
+$pmp = new SupraCsvPostMetaPreset();
+$pm = new SupraCsvPostMeta();
+
 wp_enqueue_script( 'inputCloner', plugins_url('/js/inputCloner.js', __FILE__) );
 wp_enqueue_script( 'base_preset', plugins_url('/js/base_preset.js', __FILE__) );
 wp_enqueue_script( 'postmeta_preset', plugins_url('/js/postmeta_preset.js', __FILE__) );
