07ddcdd74a2d0c95535cf1298a87d47da0cf1d87
zmijevik <zmijevik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-02 04:43:16 +0000

    2.6.3
    git-svn-id: https://plugins.svn.wordpress.org/supra-csv-parser/tags/2.6.3@593554 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- supra_csv_admin.php
+++ supra_csv_admin.php
@@ -1,5 +1,4 @@
 <?php 
-
 require_once(dirname(__FILE__).'/classes/SupraCsvPlugin.php');
 
 $scp = new SupraCsvPlugin();
@@ -14,11 +13,17 @@ if(!empty($_POST['scsv_submit'])) {
     $csvpost['desc'] = $_POST['scsv_defaultdesc'];
     $post_terms = $_POST['scsv_custom_terms'];
     $parse_terms = $_POST['scsv_parse_terms'];
+    $ingest_debugger = $_POST['scsv_ingest_debugger'];
+    $csv_settings = $_POST['scsv_csv_settings'];
+    $report_issue = $_POST['scsv_report_issue'];
     update_option('scsv_filename', $csvfile);
     update_option('scsv_user', $csvuser);
     update_option('scsv_post', $csvpost);
     update_option('scsv_custom_terms', $post_terms);
     update_option('scsv_parse_terms', $parse_terms);
+    update_option('scsv_ingest_debugger', $ingest_debugger);
+    update_option('scsv_report_issue', $report_issue);
+    update_option('scsv_csv_settings', $csv_settings);
     echo '<div class="updated"><p><strong>Configuration saved</strong></p></div>';
 } else {
     $csvfile = get_option('scsv_filename');
@@ -26,44 +31,18 @@ if(!empty($_POST['scsv_submit'])) {
     $csvpost = get_option('scsv_post');
     $post_terms = get_option('scsv_custom_terms');
     $parse_terms = get_option('scsv_parse_terms');
+    $ingest_debugger = get_option('scsv_ingest_debugger');
+    $report_issue = get_option('scsv_report_issue');
+    $csv_settings = get_option('scsv_csv_settings');
 }
+
 ?>
-<div class="wrap">
-<h2>Supra Csv Importer</h2>
-<h3>Description</h3>
-<p>The purpose of this plugin is to parse uploaded csv files into any type of post.
-Some themes or plugin store data in post with custom post_type, thus this plugin
-provides the functionality to upload data from the csv file to the records that 
-the theme or plugin creates. Manage existing csv files and promote ease of use by 
-creating presets for both postmeta and ingestion mapping. For more infomation on how to obtain the necessary info
- watch the detailed tutorial. Csv Importer, Csv Parser, Csv Injector, Custom Post.</p>
-<h3>Steps to Ingest</h3>
-<ol> 
-<li>configure in 'Configuration'</li>
-<li>upload file in 'Upload'</li>
-<li>define postmeta in 'Post Info'</li>
-<li>map the data and import in 'Ingestion'</li>
-<li>save postmeta and mapping presets wherever necessary</li>
-</ol>
-</p>
-
-<h3>Importing Terms by Taxonomy</h3>
-<p>provide a comma separated value in the <a href="#custom_terms">custom terms</a> input below<br />
-Exa: enginesize,pricerange<br />
-The mapping selecotrs will dynamically appear in the ingest page.
-</p>
-
-<h3>Tutorial</h3>
-<iframe width="420" height="315" src="http://www.youtube.com/embed/0xKpNw1cT-Q" frameborder="0" allowfullscreen></iframe>
-            <h3>Contributions</h3>
-            <p>Due to the nature of wordpress and its lack of plugin contribution functionality please fork the following repo.</p>
-            <p><a href="https://github.com/zmijevik/Supra-CSV" target="_blank">https://github.com/zmijevik/Supra-CSV</a></p>
-            <p>Please keep code encapsulated, lets keep this from turning into a procedural mess!</p>
-            <p>You have my guarantee that this project will <u>remain</u> free and open source.</p>
-            <p>Feel free to contact me regarding plugin issues and requests.</p>
-
+<div style="width: 630px">
 <h2>Supra CSV Configuration</h2>
-<form name="scsv_form" method="post"">
+<div style="float: left; width: 300px;">
+
+<form name="scsv_form" method="post">
+        <hr />
         <h4>User Settings</h4>
 	<p>Username<input type="text" name="scsv_wpname" value="<?php echo $csvuser['name']; ?>" size="20"></p>
 	<p>Pasword<input type="password" name="scsv_wppass" value="<?php echo $csvuser['pass']; ?>" size="20"></p>
@@ -89,6 +68,12 @@ The mapping selecotrs will dynamically appear in the ingest page.
             Custom Post Type
             <input type="text" name="scsv_custom_posttype" value="<?php if($csvpost['type']!="page"&&$csvpost['type']!="post")echo $csvpost['type']; ?>" size="20">
         </p>
+	<p>Default Title<input type="text" name="scsv_defaulttitle" value="<?php echo $csvpost['title']; ?>" size="20"></p>
+	<p>Default Description<textarea name="scsv_defaultdesc"><?php echo $csvpost['desc']; ?></textarea></p>
+        <hr />
+</div>
+<div style="float: right; width: 300px;">
+        <h4>Ingestion Settings</h4>
         <p id="custom_terms">
           Custom Terms (<span style="color: red">separate terms with commas</span>)
             <input type="text" name="scsv_custom_terms" value="<?=$post_terms?>" size="50">
@@ -96,10 +81,24 @@ The mapping selecotrs will dynamically appear in the ingest page.
         <p id="compex_categories">
             Parse complex categories: <input type="checkbox" name="scsv_parse_terms" value="true" <?=($parse_terms)?'checked="checked"':''?>>
         </p>
-	<p>Default Title<input type="text" name="scsv_defaulttitle" value="<?php echo $csvpost['title']; ?>" size="20"></p>
-	<p>Default Description<textarea name="scsv_defaultdesc"><?php echo $csvpost['desc']; ?></textarea></p>
-	<p class="submit">
-	<input type="submit" name="scsv_submit" value="Update Options" />
-	</p>
+        <p id="ingestion_debugging">
+            Debug Ingestion: <input type="checkbox" name="scsv_ingest_debugger" value="true" <?=($ingest_debugger)?'checked="checked"':''?>>
+        </p>
+        <p id="issue_reporting">
+            Report Issues: <input type="checkbox" name="scsv_report_issue" value="true" <?=($report_issue)?'checked="checked"':''?>>
+        </p>
+        <hr />
+        <h4>CSV Settings</h4>
+        <p id="csv_settings">
+            <? $settings_keys = array('delimiter'=>',','enclosure'=>'"','escape'=>'\\'); ?>
+            <? foreach($settings_keys as $k=>$v): ?>
+                <?=$k?>:<input type='text' name='scsv_csv_settings[<?=$k?>]' value='<?=($csv_settings[$k])?stripslashes($csv_settings[$k]):$v;?>' size='2' maxlength='2' /><br />
+            <? endforeach; ?>
+        </p>
+        <hr />
+        <p class="submit">
+            <input type="submit" name="scsv_submit" value="Update Options" />
+        </p>
 </form>
+</div>
 </div>
