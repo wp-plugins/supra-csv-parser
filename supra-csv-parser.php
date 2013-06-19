6ddbf48dded49dda67990bfaf7ffa1bc796e5e02
zmijevik <zmijevik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-19 00:12:29 +0000

    3.0.1
    git-svn-id: https://plugins.svn.wordpress.org/supra-csv-parser/tags/3.0.1@728105 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- supra-csv-parser.php
+++ supra-csv-parser.php
@@ -2,7 +2,7 @@
 /*
    Plugin Name: Supra CSV
    Plugin URI: http://wordpress.org/extend/plugins/supra-csv-parser/
-   Version: 2.9.9
+   Version: 3.0.1
    Description: A plugin for parsing and extracting csv files from posts.
    Author: zmijevik
    Text Domain: supracsvparser
